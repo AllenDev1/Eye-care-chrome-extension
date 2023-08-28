bd51315618cfe561b342874333d2244ab7e06229
AllenDev1 <dev.allen252@gmail.com>
2023-08-28 20:13:22 +0545

    :bug: bugs added

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
-chrome.alarms.create({ delayInMinutes: 30, periodInMinutes: 30 });
+console.log("popup.js");
+chrome.alarms.create({ delayInMinutes: 1, periodInMinutes: 1 });
 
-chrome.alarms.onAlarm.addListener(alarm => {
-  chrome.action.setBadgeText({ text: "!" });
+chrome.alarms.onAlarm.addListener((alarm) => {
+	chrome.action.setBadgeText({ text: "!" });
 });
